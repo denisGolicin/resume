5e5b0554bbcaac1007646a7d8e5e01d99175b523
denisGolicin <den.golicin65@gmail.com>
2022-12-06 20:27:57 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const about = document.getElementById("about"); 
+const project = document.getElementById("project");
+const mainContent = document.getElementById("main-content");
+const projectContent = document.getElementById("project-content");
+
+setTimeout(scrollEnable, 8000);
+
+function scrollEnable() {
+    document.getElementById("main-wrapper").style.overflowY = "scroll";
+    document.getElementById("main-wrapper").style.maxHeight = "2000px";
+}
+
+project.addEventListener("click", () => {
+    console.log("project");
+    if(project.classList.contains("active")){
+        console.log("project active");
+    }
+    else {
+        project.classList.add("active");
+        about.classList.remove("active");
+        mainContent.style.display = "none";
+        projectContent.style.display = "block";
+        projectContent.style.animation = "animation-main-content 1s forwards";
+    }
+}); 
+about.addEventListener("click", () => {
+    console.log("about");
+    if(about.classList.contains("active")){
+        console.log("about active");
+    }
+    else {
+        project.classList.remove("active");
+        about.classList.add("active");
+        mainContent.style.display = "block";
+        projectContent.style.display = "none";
+        mainContent.style.animationDelay = "0s";
+        
+
+    }
+}); 
+const boxes = Array.from(document.querySelectorAll(".project-accordion")); 
+
+boxes.forEach((box) => {
+    box.addEventListener("click", boxHandler); 
+    console.log(box);
+});
+
+function boxHandler(e) {
+    // e.preventDefault(); 
+    let currentBox = e.target.closest(".project-accordion"); 
+    let currentContent = currentBox.lastElementChild; 
+    if(e.target == e.target.closest(".tedt")){
+        console.log("No");
+        return;
+    }
+    console.log(e.target);
+    currentBox.classList.toggle("project-active"); 
+    if (currentBox.classList.contains("project-active")) {
+        // если класс активный ..
+        currentContent.style.maxHeight = currentContent.scrollHeight + "px"; 
+    } else {
+        // в противном случае
+        currentContent.style.maxHeight = 0; 
+    }
+}
